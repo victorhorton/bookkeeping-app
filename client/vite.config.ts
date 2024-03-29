1fe0ac8bb4777ff23faea5a3859c19ce9a88ad2a
Victor Horton <vichorton5@gmail.com>
2024-03-29 12:09:55 -0500

    Use proxy server for development

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,5 +13,12 @@ export default defineConfig({
   },
   build: {
     outDir: '../wwwroot'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5054'
+      }
+    }
   }
 })
