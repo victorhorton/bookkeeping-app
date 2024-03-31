1ff1c8bfbd0129f1bc1650ada55b648e50ce88f5
Victor Horton <vichorton5@gmail.com>
2024-03-31 17:58:49 -0500

    Add company model and migration

---
+++ Models/Company.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace bookkeeping_app.Models;
+public class Company
+{
+    [Key]
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Code { get; set; }
+}
