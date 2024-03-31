c770d7fbc0e6c22f038d84d317de23bcea11f1b6
Victor Horton <vichorton5@gmail.com>
2024-03-31 17:53:13 -0500

    Add account model and migration

---
+++ Models/Account.cs
@@ -0,0 +1,11 @@
+// Models/User.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace bookkeeping_app.Models;
+public class Account
+{
+    [Key]
+    public int Id { get; set; }
+    public string? Number { get; set; }
+    public string? Name { get; set; }
+}
